1189f0c1e981bcc58d8416a87e207cb1b23912d7
Jack Zhang <jackzhang@dynamic-oit-ip4-wifirestricted03-10-49-241-163.princeton.edu>
2025-04-25 21:42:28 -0400

    debug

--- llm_judge/gen_model_answer_adaptive.py
+++ llm_judge/gen_model_answer_adaptive.py
@@ -316,6 +316,7 @@ def get_model_answers(
                         output_ids,
                         spaces_between_special_tokens=False,
                     )
+                    print("here")
                     print(output)
                     if conv.stop_str and output.find(conv.stop_str) > 0:
                         output = output[: output.find(conv.stop_str)]
