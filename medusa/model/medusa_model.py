030a77a331100e81a63d02df84af4c56a6e6bf16
Jack Zhang <jackzhang@dynamic-oit-ip4-wifirestricted03-10-49-241-163.princeton.edu>
2025-04-26 18:04:23 -0400

    m

--- medusa/model/medusa_model.py
+++ medusa/model/medusa_model.py
@@ -145,8 +145,8 @@ class MedusaModelABC(nn.Module):
             print(config.to_dict())
             base_model_config = AutoConfig.from_pretrained(config.base_model_name_or_path)
             print(base_model_config.to_dict())
-            #base_model_config.medusa_num_heads = 5 # TODO: fix the uploaded config (only include 2 heads)
-            base_model_config.medusa_num_heads = 4
+            base_model_config.medusa_num_heads = 5 # TODO: fix the uploaded config (only include 2 heads)
+            #base_model_config.medusa_num_heads = 4
             base_model_config.medusa_num_layers = config.medusa_num_layers
             model = super().from_pretrained(
                 config.base_model_name_or_path,
